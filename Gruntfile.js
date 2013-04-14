130822ced0808ae33d4119232af43526757bea5d
Thanasis Polychronakis <thanpolas@gmail.com>
2013-04-14 03:59:31 +0300

    automating release

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,11 +20,22 @@ module.exports = function (grunt) {
         tasks: ['']
       }
     },
-    // clean, uglify and concat aid in building
-    clean: {
-      dist: ['temp'],
-      server: 'temp'
+    release: {
+      options: {
+        bump: true, //default: true
+        file: 'package.json', //default: package.json
+        add: true, //default: true
+        commit: true, //default: true
+        tag: true, //default: true
+        push: true, //default: true
+        pushTags: true, //default: true
+        npm: true, //default: true
+        tagName: 'v<%= version %>', //default: '<%= version %>'
+        commitMessage: 'releasing v<%= version %>', //default: 'release <%= version %>'
+        tagMessage: 'v<%= version %>' //default: 'Version <%= version %>'
+      }
     }
+
   });
 
 
